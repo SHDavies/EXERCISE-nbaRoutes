87fdccc930fd3944616f6c1129a2aa1b619de4b7
Spencer Davies <spencerhdavies@gmail.com>
2015-07-27 17:40:17 -0600

    finished project

--- js/home/homeCtrl.js
+++ js/home/homeCtrl.js
@@ -1,5 +1,16 @@
 var app = angular.module('nbaRoutes');
 
-app.controller('homeCtrl', function($scope, homeService){
+app.controller('homeCtrl', function($scope, homeService, teamService){
 
+	$scope.getAllData = function() {
+		teamService.getTeamData('utahjazz').then(function(response) {
+			$scope.jazzData = response;
+		})
+		teamService.getTeamData('losangeleslakers').then(function(response) {
+			$scope.lakersData = response;
+		})
+		teamService.getTeamData('miamiheat').then(function(response) {
+			$scope.heatData = response;
+		})
+	}();
 });
